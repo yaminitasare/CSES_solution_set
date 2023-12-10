3e1214d2705d2bd11f7f2141b9d713db4e934e65
Yamini Tasare <119790229+yaminitasare@users.noreply.github.com>
2023-12-10 22:18:11 +0530

    Update trailingzeroes.cpp

--- Introductory_Problems/trailingzeroes.cpp
+++ Introductory_Problems/trailingzeroes.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
 using namespace std;
 void num_zeroes(int k,int m){
-    
+if(k>0){
+    int y = k;
+    while(y%5==0){
+        m++;
+        y = y/5;
+    }
+    num_zeroes(k-5,m);
+}
+else{
+    cout<<m;
+}
 }
 int main(){
     long long int n;
     cin>>n;
     int m = 0;
-    num_zeroes(n,m);
+    int p = n - n%5;
+    num_zeroes(p,m);
 }
